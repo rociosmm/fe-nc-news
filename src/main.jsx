6a6575e77c0db6d9e7050823d9bccb2ad5b9a8ef
Rocio <rociosmm@yahoo.com>
2025-03-06 18:10:58 +0000

    header links edited for subdirectory url

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import {BrowserRouter} from "react-router-dom";
 import {UserProvider} from "./context/UserContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<BrowserRouter>
+	<BrowserRouter basename="/nc-news">
 		<UserProvider>
 			<App />
 		</UserProvider>
