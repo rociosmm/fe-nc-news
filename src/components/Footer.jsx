7d44d428ec24c9930d2a627e5d19620a35d25fd7
Rocio <rociosmm@yahoo.com>
2025-03-06 21:05:50 +0000

    fix css footer mobile

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
 export const Footer = ({topics}) => {
 	return (
 		<footer className="p-3 mt-4 mx-0 bg-body-tertiary">
-			<div className="container">
-				<div className="logo">
+			<div className="container row">
+				<div className="logo col-10 col-md-3 offset-1 offset-md-0 text-center text-md-start">
 					<span className="news">N</span>C<span className="news">News</span>
 				</div>
-				<div id="footer-nav">
-					<ul>
-						<li>
+				<div id="footer-nav" className="col-12 col-md-9 d-md-flex">
+					<ul className="ms-md-auto ps-0 align-middle mb-0">
+						<li className="d-none d-md-block">
 							<Link to="/">Home</Link>
 						</li>
 						<li>
